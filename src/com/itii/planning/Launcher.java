acdf80d141cfb2ba7b5c4787682d9579f2defff6
monnier <monnier.jacques.lgm@gmail.com>
2019-04-27 16:38:51 +0200

    Update Launcher.java and MainWindow.java
    Able to run the window (SINGLETONED)

--- src/com/itii/planning/Launcher.java
+++ src/com/itii/planning/Launcher.java
@@ -2,10 +2,13 @@ package com.itii.planning;
 
 import javax.swing.JFrame;
 
+import com.itii.planning.gui.MainWindow;
+
+
 public class Launcher
-{ 
+{
   public static void main(String[] args)
   {
-    
+    MainWindow.getInstance();
   }
 }
--- src/com/itii/planning/gui/MainWindow.java
+++ src/com/itii/planning/gui/MainWindow.java
@@ -2,7 +2,36 @@ package com.itii.planning.gui;
 
 import javax.swing.JFrame;
 
+
+
 public class MainWindow extends JFrame
 {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
 
+  public static MainWindow INSTANCE;
+
+  public static MainWindow getInstance(){
+    if(INSTANCE == null) {
+      INSTANCE = new MainWindow();
+     // INSTANCE.initialize();
+    }
+    return INSTANCE;
+  }
+
+  private MainWindow()
+  {
+    initialize();
+  }
+
+  public void initialize()
+  {
+
+      this.setSize(600,480);
+
+
+      this.setVisible(true);
+  }
 }
