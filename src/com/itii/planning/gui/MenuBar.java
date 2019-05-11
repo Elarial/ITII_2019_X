effdea0f4235b8cf9d77ac847abcc19e9b35182c
monnier <monnier.jacques.lgm@gmail.com>
2019-05-11 14:31:01 +0200

    Add item for each menu list

--- src/com/itii/planning/gui/MenuBar.java
+++ src/com/itii/planning/gui/MenuBar.java
@@ -5,6 +5,7 @@ package com.itii.planning.gui;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * @author Jacques
@@ -29,6 +30,30 @@ public class MenuBar extends JMenuBar
    JMenu menuView    =  new JMenu("Vue");
    JMenu menuAide    =  new JMenu("Aide");
    
+   JMenuItem itemFichierQuitter = new JMenuItem("Quitter");
+   
+   JMenuItem itemEditionCreer     = new JMenuItem("Créer");
+   JMenuItem itemEditionEditer    = new JMenuItem("Editer");
+   JMenuItem itemEditionMarquer   = new JMenuItem("Marquer");
+   JMenuItem itemEditionDupliquer = new JMenuItem("Dupliqer");
+   JMenuItem itemEditionSupprimer = new JMenuItem("Supprimer");
+   
+   JMenuItem itemVueListe   = new JMenuItem("Liste");
+   JMenuItem itemVueSemaine = new JMenuItem("Semaine");
+   JMenuItem itemVueMois    = new JMenuItem("Mois");
+   
+   
+   menuFichier.add(itemFichierQuitter);
+   
+   menuEdition.add(itemEditionCreer);
+   menuEdition.add(itemEditionEditer);
+   menuEdition.add(itemEditionMarquer);
+   menuEdition.add(itemEditionDupliquer);
+   menuEdition.add(itemEditionSupprimer);
+   
+   menuView.add(itemVueListe);
+   menuView.add(itemVueSemaine);
+   menuView.add(itemVueMois);
    
    this.add(menuFichier);
    this.add(menuEdition);
